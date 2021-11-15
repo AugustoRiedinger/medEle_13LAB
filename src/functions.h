facc50b491c7134ff326d96c00f6307c62f3af76
Augusto Riedinger <riedinger.augusto@gmail.com>
2021-11-15 11:39:02 -0300

    Se añadieron todas las declaraciones a function.h

--- src/functions.h
+++ src/functions.h
@@ -1,8 +1,18 @@
+/********************************************************************************
+  * @file    functions.h
+  * @author  A. Riedinger & G. Stang.
+  * @version 0.1
+  * @date    09-11-21.
+  * @brief   Definicion de librerias y valores para main.c
+********************************************************************************/
+
 /* Definicion del header:*/
 #ifndef functions_H
 #define functions_H
 
-/*Librerias para habilitar funciones internas de la STM:*/
+/*------------------------------------------------------------------------------
+LIBRERIAS:
+------------------------------------------------------------------------------*/
 #include "stm32f4xx.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
@@ -12,31 +22,27 @@
 #include "misc.h"
 #include "stm32f4xx_syscfg.h"
 #include "stm32f4xx_dma.h"
-#include "connections.h"
 
+/*------------------------------------------------------------------------------
+DECLARACION DE ESTRUCTURAS:
+------------------------------------------------------------------------------*/
 TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
 TIM_OCInitTypeDef  		TIM_OCInitStructure;
 EXTI_InitTypeDef   		EXTI_InitStructure;
 DAC_InitTypeDef 		DAC_InitStructure;
 
-//--------------------------------------------------------------
-// Defines
-//--------------------------------------------------------------
+/*------------------------------------------------------------------------------
+DEFINICION CONSTANTES LCD:
+------------------------------------------------------------------------------*/
 #define  TLCD_INIT_PAUSE  100000  // pause beim init (>=70000)
 #define  TLCD_PAUSE        50000  // kleine Pause (>=20000)
-#define  TLCD_CLK_PAUSE     1000  // pause f�r Clock-Impuls (>=500)
+#define  TLCD_CLK_PAUSE     1000  // pause for Clock-Impuls (>=500)
 #define  TLCD_MAXX            16  // max x-Position (0...15)
 #define  TLCD_MAXY             2  // max y-Position (0...1)
-#define  Delay_Debouncing 100e3
-#define	 BufferLength 	  20
 #define  MaxDigCount 	  4095
 #define  MaxMiliVoltRef	  3320
 #define  Res32Bit		  32
 #define  DAC_DHR12R2_ADDRESS   0x40007414
-
-//--------------------------------------------------------------
-// LCD Kommandos (siehe Datenblatt)
-//--------------------------------------------------------------
 #define  TLCD_CMD_INIT_DISPLAY  0x28   // 2 Zeilen Display, 5x7 Punkte
 #define  TLCD_CMD_ENTRY_MODE    0x06   // Cursor increase, Display fix
 #define  TLCD_CMD_DISP_M0       0x08   // Display=AUS, Cursor=Aus, Blinken=Aus
@@ -45,10 +51,6 @@ DAC_InitTypeDef 		DAC_InitStructure;
 #define  TLCD_CMD_DISP_M3       0x0F   // Display=EIN, Cursor=EIN, Blinken=EIN
 #define  TLCD_CMD_CLEAR         0x01   // loescht das Display
 
-//--------------------------------------------------------------
-// Liste aller Pins fur das Display
-// (keine Nummer doppelt und von 0 beginnend)
-//--------------------------------------------------------------
 typedef enum
 {
   TLCD_RS = 0,  // RS-Pin
@@ -61,18 +63,13 @@ typedef enum
 
 #define  TLCD_ANZ   6 // Anzahl von TLCD_NAME_t
 
-//--------------------------------------------------------------
-// Display Modes
-//--------------------------------------------------------------
 typedef enum {
   TLCD_OFF = 0,    // Display=AUS, Cursor=Aus, Blinken=Aus
-  TLCD_ON,        // Display=EIN, Cursor=Aus, Blinken=Aus
-  TLCD_CURSOR,    // Display=EIN, Cursor=EIN, Blinken=Aus
-  TLCD_BLINK      // Display=EIN, Cursor=EIN, Blinken=EIN
+  TLCD_ON,         // Display=EIN, Cursor=Aus, Blinken=Aus
+  TLCD_CURSOR,     // Display=EIN, Cursor=EIN, Blinken=Aus
+  TLCD_BLINK       // Display=EIN, Cursor=EIN, Blinken=EIN
 }TLCD_MODE_t;
 
-
-/*Definicion de los pines del LCD:*/
 typedef struct {
   TLCD_NAME_t TLCD_NAME;   // Name
   GPIO_TypeDef* TLCD_PORT; // Port
@@ -81,22 +78,9 @@ typedef struct {
   BitAction TLCD_INIT;     // Init
 }LCD_2X16_t;
 
-
-
-
-/*Declaracion funciones:*/
-void 	INIT_DO(GPIO_TypeDef* Port, uint32_t Pin);
-void 	INIT_ADC(void);
-void 	ADC_INIT(void);
-int32_t READ_ADC1(void);
-int32_t READ_ADC2(void);
-void 	INIT_TIM3();
-void 	SET_TIM3(uint32_t TimeBase, uint32_t Freq);
-void	INIT_LCD_2x16(LCD_2X16_t*);
-void	CLEAR_LCD_2x16(LCD_2X16_t*);
-void	PRINT_LCD_2x16(LCD_2X16_t*, uint8_t, uint8_t, char*);
-
-
+/*------------------------------------------------------------------------------
+DEFINICION PINES DEL HARDWARE:
+------------------------------------------------------------------------------*/
 /*Pines del ADC para medir tension - PC0:*/
 #define _adcVoltPort	GPIOC
 #define _adcVoltPin		GPIO_Pin_0
@@ -115,5 +99,82 @@ LCD_2X16_t LCD_2X16[] = {
 			{ TLCD_D6, GPIOF, GPIO_Pin_6,  RCC_AHB1Periph_GPIOF, Bit_RESET },
 			{ TLCD_D7, GPIOF, GPIO_Pin_7,  RCC_AHB1Periph_GPIOF, Bit_RESET }, };
 
-/* Cierre del header:*/
+/*------------------------------------------------------------------------------
+DEFINICION DE VALORES PARA MAIN.C:
+------------------------------------------------------------------------------*/
+/*Base de tiempo para el TIM3:*/
+#define timeBase 200e3 //[mseg]
+
+/*Frecuencia de muestreo para configurar el TIM3:*/
+#define FS  5000 //[Hz]
+
+/*Frecuencia base de la red:*/
+#define baseFreq 50 //[Hz]
+
+/*Maximo del buffer para el LCD:*/
+#define lcdBufferLen 20
+
+/*Voltaje maximo analogico:*/
+#define maxVoltValue 340 //[Vpp]
+
+/*Corriente maxima analogica:*/
+#define maxCurrValue 180 //[mA]
+
+/*Valor maximo digital al voltaje o corriente maximo:*/
+#define maxDigValue 4095
+
+/*Maximo almacenamiento en un ciclo:*/
+#define maxSampling FS / baseFreq
+
+/*Ingreso al clear del display cada 200mseg:*/
+#define ticksLCD	1000
+
+/*------------------------------------------------------------------------------
+DECLARACION FUNCIONES GLOBALES DE MAIN.C:
+------------------------------------------------------------------------------*/
+/*Procesamiento de datos del LCD:*/
+void ADC_PROCESSING(void);
+
+/*Manejo del LCD:*/
+void LCD(void);
+
+/*Calcular la potencia activa:*/
+void P(void);
+
+/*------------------------------------------------------------------------------
+DECLARACION VARIABLES GLOBALES DE MAIN.C:
+------------------------------------------------------------------------------*/
+/*Variables de manejo del TS:*/
+uint32_t adc = 0;
+uint32_t lcd = 0;
+
+/*Variable para controlar el almacenamiento de datos de los ADC:*/
+uint32_t instant = 0;
+
+/*Variables de almacenamiento de datos del ADC en forma digital:*/
+uint32_t voltValueDig[maxSampling];
+uint32_t currValueDig[maxSampling];
+
+/*Variables de almacenamiento de datos del ADC en forma analogica:*/
+float 	 voltValueAna[maxSampling];
+float 	 currValueAna[maxSampling];
+
+/*Variable para almacenar la potencia activa:*/
+float 	 activePow = 0.0f;
+
+/*------------------------------------------------------------------------------
+DECLARACION FUNCIONES DE FUNCTIONS.C :
+------------------------------------------------------------------------------*/
+void 	INIT_DO(GPIO_TypeDef* Port, uint32_t Pin);
+void 	INIT_ADC(void);
+void 	ADC_INIT(void);
+int32_t READ_ADC1(void);
+int32_t READ_ADC2(void);
+void 	INIT_TIM3();
+void 	SET_TIM3(uint32_t TimeBase, uint32_t Freq);
+void	INIT_LCD_2x16(LCD_2X16_t*);
+void	CLEAR_LCD_2x16(LCD_2X16_t*);
+void	PRINT_LCD_2x16(LCD_2X16_t*, uint8_t, uint8_t, char*);
+
+/*Cierre del header:*/
 #endif
--- src/main.c
+++ src/main.c
@@ -13,68 +13,7 @@
   	  * TEC-MAT
 ********************************************************************************/
 
-/*------------------------------------------------------------------------------
-LIBRERIAS:
-------------------------------------------------------------------------------*/
 #include "functions.h"
-
-/*------------------------------------------------------------------------------
-DEFINICIONES LOCALES:
-------------------------------------------------------------------------------*/
-/*Base de tiempo para el TIM3:*/
-#define timeBase 200e3 //[mseg]
-
-/*Frecuencia de muestreo segun el TIM3:*/
-#define FS  5000 //[Hz]
-
-/*Frecuencia base de la red:*/
-#define baseFreq 50 //[Hz]
-
-/*Voltaje maximo analogico:*/
-#define maxVoltValue 340 //[Vpp]
-
-/*Corriente maxima analogica:*/
-#define maxCurrValue 180 //[mA]
-
-/*Valor maximo digital al voltaje o corriente maximo:*/
-#define maxDigValue 4095
-
-/*Ticks del TS:*/
-/*Se toma un dato del ADC en cada instante de muestreo:*/
-#define ticksADC	1
-/*Se clarea el display cada 200mseg:*/
-#define ticksLCD	1000
-
-/*Tareas del TS:*/
-void ADC_PROCESSING(void);
-void LCD(void);
-
-/*Funcion para calcular la potencia activa:*/
-void P(void);
-
-/*------------------------------------------------------------------------------
-VARIABLES GLOBALES:
-------------------------------------------------------------------------------*/
-/*Variables de manejo del TS:*/
-uint32_t adc = 0;
-uint32_t lcd = 0;
-
-/*Variable para controlar el almacenamiento de datos de los ADC:*/
-uint32_t instant = 0;
-
-/*Maximo de almacenamiento en un ciclo:*/
-uint32_t maxSampling = FS / baseFreq;
-
-/*Variables de almacenamiento de datos del ADC en forma digital:*/
-uint32_t voltValueDig[maxSampling];
-uint32_t currValueDig[maxSampling];
-
-/*Variables de almacenamiento de datos del ADC en forma analogica:*/
-float 	 voltValueAna[maxSampling];
-float 	 currValueAna[maxSampling];
-
-/*Variable para almacenar la potencia activa:*/
-float 	 activePow = 0.0f;
 
 int main(void)
 {
@@ -124,6 +63,29 @@ void TIM3_IRQHandler(void) {
 /*------------------------------------------------------------------------------
 TAREAS:
 ------------------------------------------------------------------------------*/
+/*Mostrar datos en el LCD:*/
+void LCD(void)
+{
+	/*Reseteo de la variable del TS:*/
+	lcd = 0;
+
+	/*Calculo de la potencia activa:*/
+	P();
+
+	/*Buffers para mostrar valores de variables:*/
+	char buffActivePow[lcdBufferLen];
+
+	/*Refresco del LCD:*/
+	CLEAR_LCD_2x16(LCD_2X16);
+
+	/*Mostrar potencia activa:*/
+	sprintf(buffActivePow, "P=%.1f W", activePow);
+	PRINT_LCD_2x16(LCD_2X16, 0, 0, buffActivePow);
+
+	/*Reseteo de las variables de calculo de potencia:*/
+	activePow = 0.0f;
+}
+
 /*Tomar muestras de tension y corriente mediante los ADC:*/
 void ADC_PROCESSING(void)
 {
@@ -146,29 +108,6 @@ void ADC_PROCESSING(void)
 	/*Sino, se sigue aumentando:*/
 	else
 		instant++;
-}
-
-/*Mostrar datos en el LCD:*/
-void LCD(void)
-{
-	/*Reseteo de la variable del TS:*/
-	lcd = 0;
-
-	/*Calculo de la potencia activa:*/
-	P();
-
-	/*Buffers para mostrar valores de variables:*/
-	char buffActivePow[BufferLength];
-
-	/*Refresco del LCD:*/
-	CLEAR_LCD_2x16(LCD_2X16);
-
-	/*Mostrar potencia activa:*/
-	sprintf(buffActivePow, "P=%.1f W", activePow);
-	PRINT_LCD_2x16(LCD_2X16, 0, 0, buffActivePow);
-
-	/*Reseteo de las variables de calculo de potencia:*/
-	activePow = 0.0f;
 }
 
 /*Calculo de la potencia activa:*/
